e9c8a6a3bb177ad87223c0160d1f8aeec9193822
Justin Cha <sungsoocha97@gmail.com>
2023-03-03 10:38:48 -0500

    Added comments

--- js/parse_composite.js
+++ js/parse_composite.js
@@ -4,24 +4,29 @@ let parse_composite = function(text) {
         i = 0;
     while (i < lines.length) {
         let line = lines[i];
+        // Skip empty lines
         if (line.trim() === "") {
             i++;
             continue
         };
+        // Tab-separated fields
         let fields = line.split("\t");
+        // If the first field is empty or "NAME" it is the x domain
         if (fields[0] === "" || fields[0] === "NAME") {
             xmin_curr = parseInt(fields[1]);
             xmax_curr = parseInt(fields[fields.length - 1]);
+            // If the x domain starts at 0 shift it to the left
             if (xmin_curr === 0) {
                 xmin_curr -= Math.floor(xmax_curr / 2)
                 xmax_curr -= Math.floor(xmax_curr / 2)
             };
-
+            // If the x domain is not defined yet, define it
             if (xmin === undefined || xmax === undefined) {
                 xmin = xmin_curr;
                 xmax = xmax_curr;
                 sense = Array(xmax_curr - xmin_curr + 1).fill(0);
                 anti = Array(xmax_curr - xmin_curr + 1).fill(0)
+            // If the x domain is defined, extend it if necessary
             } else {
                 if (xmin_curr < xmin) {
                     sense.unshift(Array(xmin - xmin_curr).fill(0));
@@ -34,8 +39,9 @@ let parse_composite = function(text) {
                     xmax = xmax_curr
                 }
             };
-
+            // Calculate the offset of the current x domain
             offset = xmin_curr - xmin
+        // Add the values to the sense and anti arrays
         } else if (fields[0].toLowerCase().includes("sense")) {
             for (let j = 1; j < fields.length; j++) {
                 sense[offset + j - 1] += parseFloat(fields[j])
@@ -44,6 +50,7 @@ let parse_composite = function(text) {
             for (let j = 1; j < fields.length; j++) {
                 anti[offset + j - 1] += parseFloat(fields[j])
             }
+        // If the first field is not empty or "NAME" and does not contain "sense" or "anti" it is combined or midpoint data
         } else {
             for (let j = 1; j < fields.length; j++) {
                 sense[offset + j - 1] += parseFloat(fields[j]) / 2;
