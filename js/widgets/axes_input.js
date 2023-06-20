98e2437777f0a73d0c50902affd3c02ce126c32c
Justin Cha <sungsoocha97@gmail.com>
2023-06-20 13:01:23 -0400

    Fixed small combinatorial bug with how axis limit boxes are enabled/disabled and added comments

--- js/widgets/axes_input.js
+++ js/widgets/axes_input.js
@@ -6,6 +6,11 @@ $(function() {
         xmax: 500,
         ymin: -1,
         ymax: 1,
+
+        // Flag to indicate whether axes are locked
+        locked: false,
+
+        // Flag to indicate whether the strands are combined
         combined: false,
 
         _elements: {
@@ -16,6 +21,7 @@ $(function() {
         },
 
         _create: function() {
+            // Create container for axis controls
             let container = d3.select(this.element.context),
                 xaxis = container.append("div")
                     .attr("class", "row")
@@ -26,6 +32,7 @@ $(function() {
                     .append("div")
                         .attr("class", "col");
 
+            // Create x axis controls
             xaxis.append("label")
                 .attr("for", "xmin-text")
                 .text("x axis limits:");
@@ -45,6 +52,7 @@ $(function() {
                 .style("margin-left", "5px")
                 .on("change", function() {$("#axes-input").axes_input("change_axis_limits", null, this.value, null, null)});
 
+            // Create y axis controls
             yaxis.append("label")
                 .attr("for", "ymin-text")
                 .text("y axis limits:");
@@ -66,13 +74,16 @@ $(function() {
         },
 
         change_axis_limits: function(xmin, xmax, ymin, ymax, change_plot=true) {
+            // Change x axis limits
             if (xmin !== null) {
                 this.xmin = xmin;
             };
             if (xmax !== null) {
                 this.xmax = xmax;
             };
+            // Change y axis limits
             if (this.combined && change_plot) {
+                // If the strands are combined, the y axis limits are scaled relative to the difference
                 if (ymax !== null) {
                     let factor = ymax / (this.ymax - this.ymin);
                     this.ymin = (this.ymin * factor).toPrecision(2);
@@ -87,11 +98,14 @@ $(function() {
                 }
             };
 
+            // Change the text boxes
             this._elements.xmin_text.node().value = this.xmin;
             this._elements.xmax_text.node().value = this.xmax;
+            // If the strands are combined, the y axis limit text boxes show 0 for the lower limit and the difference for the upper limit
             this._elements.ymin_text.node().value = this.combined ? 0 : this.ymin;
             this._elements.ymax_text.node().value = this.combined ? this.ymax - this.ymin : this.ymax;
 
+            // Change the plot, if requested
             if (change_plot) {
                 $("#main-plot").main_plot("scale_axes", this.xmin, this.xmax, this.ymin, this.ymax);
                 $("#settings-table").settings_table("plot_all_composites")
@@ -100,21 +114,29 @@ $(function() {
 
         toggle_combined: function(combined) {
             this.combined = combined;
+            // If the strands are combined, the y axis limit text boxes show 0 for the lower limit and the difference for the upper limit
             this._elements.ymin_text.node().value = combined ? 0 : this.ymin;
             this._elements.ymax_text.node().value = combined ? this.ymax - this.ymin : this.ymax;
 
+            // Disable the y axis lower limit text box if the strands are combined
             this._elements.ymin_text
-                .property("disabled", combined)
-                .style("background-color", combined ? "#DDDDDD" : "white")
+                .property("disabled", combined || this.locked)
+                .style("background-color", combined || this.locked ? "#DDDDDD" : "white")
         },
 
         toggle_locked: function(locked) {
+            this.locked = locked;
+
+            // Disable the text boxes if the axes are locked
             this._elements.xmin_text
                 .property("disabled", locked)
                 .style("background-color", locked ? "#DDDDDD" : "white");
             this._elements.xmax_text
                 .property("disabled", locked)
                 .style("background-color", locked ? "#DDDDDD" : "white");
+            this._elements.ymin_text
+                .property("disabled", locked || this.combined)
+                .style("background-color", locked || this.combined ? "#DDDDDD" : "white");
             this._elements.ymax_text
                 .property("disabled", locked)
                 .style("background-color", locked ? "#DDDDDD" : "white")
