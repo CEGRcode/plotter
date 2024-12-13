58e00a6d56bc4053faa9ff80248de003a5ad6e96
jcha40 <sungsoocha97@gmail.com>
2024-12-13 16:00:43 -0500

    Added settings tabs

---
+++ js/settings_tabs.js
@@ -0,0 +1,32 @@
+const settingsTabs = d3.select("#settings-tabs"),
+    globalSettingsTab = settingsTabs.append("button")
+        .attr("id", "global-settings-tab")
+        .classed("settings-tab", true)
+        .classed("selected-tab", true)
+        .text("Global settings")
+        .on("click", function() {
+            d3.selectAll(".settings-tab").classed("selected-tab", false);
+            d3.select(this).classed("selected-tab", true);
+            d3.selectAll(".settings-section").classed("hidden", true);
+            d3.select("#global-settings").classed("hidden", false)
+        }),
+    referenceAxesTab = settingsTabs.append("button")
+        .attr("id", "reference-axes-tab")
+        .classed("settings-tab", true)
+        .text("Reference axes")
+        .on("click", function() {
+            d3.selectAll(".settings-tab").classed("selected-tab", false);
+            d3.select(this).classed("selected-tab", true);
+            d3.selectAll(".settings-section").classed("hidden", true);
+            d3.select("#reference-axes").classed("hidden", false)
+        }),
+    nucleosomeSliderTab = settingsTabs.append("button")
+        .attr("id", "nucleosome-slider-tab")
+        .classed("settings-tab", true)
+        .text("Nucleosome slider")
+        .on("click", function() {
+            d3.selectAll(".settings-tab").classed("selected-tab", false);
+            d3.select(this).classed("selected-tab", true);
+            d3.selectAll(".settings-section").classed("hidden", true);
+            d3.select("#nucleosome-slider").classed("hidden", false)
+        });
